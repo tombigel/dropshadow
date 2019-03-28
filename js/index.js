369d083512c8222a2d1e08e8cbc776e7f028274c
Tom Bigelajzen <tombigel@wix.com>
2019-03-28 11:53:22 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var params = {
+  left: 10,
+  top: 10,
+  blur: 10,
+  color: 'rgba(0, 0, 0, .5)'
+};
+controls.querySelectorAll('input').forEach(function (input) {
+  var name = input.getAttribute('name');
+  input.addEventListener('change', function (event) {
+    return setParam(name, event.target.value);
+  });
+  input.value = params[name];
+  setShadow(params);
+});
+
+function setParam(key, val) {
+  params[key] = val;
+  setShadow(params);
+}
+
+function setShadow(_ref) {
+  var left = _ref.left,
+      top = _ref.top,
+      blur = _ref.blur,
+      color = _ref.color;
+  var filter = "drop-shadow(" + left + "px " + top + "px " + blur + "px " + color + ")";
+  svg.style.filter = filter;
+  circle.style.filter = filter;
+}
