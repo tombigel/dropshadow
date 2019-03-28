f3b47ee22780e3e5496745ae63a8d2a552828337
Tom Bigelajzen <tombigel@wix.com>
2019-03-28 13:50:47 +0200

    reverted ie changes

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ var params = {
   blur: 10,
   color: 'rgba(0, 0, 0, .5)'
 };
+
 controls.querySelectorAll('input').forEach(function (input) {
   var name = input.getAttribute('name');
   input.addEventListener('change', function (event) {
@@ -20,27 +21,12 @@ function setParam(key, val) {
   setShadow(params);
 }
 
-function rgbToHex(r, g, b) {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-function getIeColor(color) {
-  if (/^#[0-9,a-f,A-F]+$/.test(color)){
-    return color
-  }
-  if (/^rgba?\(/.test(color)){
-    const rgb = (color.match(/^rgba?\((\d+)\s?,\s?(\d+)\s?,\s?(\d+).*\)/) || []).slice(1,4)
-    return rgbToHex.apply(null, rgb)
-  }
-}
-
 function setShadow(_ref) {
   var left = _ref.left,
       top = _ref.top,
       blur = _ref.blur,
       color = _ref.color;
   var filter = "drop-shadow(" + left + "px " + top + "px " + blur + "px " + color + ")";
-  var msFilter = "progid:DXImageTransform.Microsoft.Dropshadow(OffX=" + left + ", OffY=" + top + ", Color='" + getIeColor(color) + "')";
 
   svg.style.filter = filter;
   circle.style.filter = filter;
